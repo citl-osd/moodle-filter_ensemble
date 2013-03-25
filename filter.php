0b5281070c38104b1d22f4c0b04f78dc11f2b1a7
Jim Pease <jmpease@codeserv.us>
2013-03-25 11:03:15 -0400

    Filter tweak.

--- filter.php
+++ filter.php
@@ -44,7 +44,7 @@ class filter_ensemble extends moodle_text_filter {
 
     foreach ($urls as $url) {
       $this->ensembleUrl = trim($url);
-      $search = '#<a href="' . $this->ensembleUrl . '\?([^"]*)".*</a>#isU';
+      $search = '#<a [^>]*href="' . $this->ensembleUrl . '\?([^"]*)".*</a>#isU';
       $newtext = preg_replace_callback($search, array('filter_ensemble', 'callback'), $newtext);
     }
 
