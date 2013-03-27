66e4dcb72570cbd07ad107aef19f5cdecb871c24
Jim Pease <jmpease@codeserv.us>
2013-03-27 13:29:50 -0400

    Updated version.

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version        = 2013030500;
+$plugin->version        = 2013032700;
 $plugin->requires       = 2012062500;
 $plugin->component      = 'filter_ensemble';
-$plugin->dependencies   = array('repository_ensemble' => 2013030500);
+$plugin->dependencies   = array('repository_ensemble' => 2013032700);
