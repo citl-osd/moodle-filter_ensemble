bca4e249a4406cfb789af5f08334c72735e65e40
Jim Pease <jmpease@codeserv.us>
2013-08-27 15:13:35 -0400

    Added reminder about version numbering for stable branch.

--- version.php
+++ version.php
@@ -11,6 +11,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+// You are on the MOODLE_24_STABLE branch.  Do NOT update the version
+// branching date rather update the release increment.
 $plugin->version        = 2013040900;
 $plugin->requires       = 2012062500;
 $plugin->component      = 'filter_ensemble';
